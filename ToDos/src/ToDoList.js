a6e6040bc1826d6205ec563c6d9d78f0db9a7186
Matthew Cook <cookm353@gmail.com>
2023-04-04 14:17:40 -0400

    Built skeleton

--- ToDos/src/ToDoList.js
+++ ToDos/src/ToDoList.js
@@ -1,10 +1,35 @@
-import React from "react"
+import React, { useState } from "react"
 import NewToDoForm from "./NewToDoForm"
 import ToDo from "./ToDo"
+import { v4 as uuidv4 } from "uuid"
 
 const ToDoList = () => {
+    const initialState = [
+        "Make breakfast",
+        "Brush teeth",
+        "Take shower",
+        "Walk dog"
+    ]
+
+    const [tasks, setTasks] = useState(initialState)
+    const addTask = newTask => {
+        setTasks(tasks => [...tasks, newTask])
+    }
+
+    const handleRemove = (e) => {
+        const button = e.target
+        
+    }
+    
     return (
         <>
+            <NewToDoForm addTask={addTask} />
+            <div>
+                {tasks.map(task => {
+                    <ToDo task={task} id={uuidv4()} />
+                })}
+                <button onClick={handleRemove}>X</button>
+            </div>
         </>
     )
 }
