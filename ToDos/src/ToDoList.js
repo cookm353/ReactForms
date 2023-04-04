8d3dcdaef19260579e1436d3546b7ec779b0fbde
Matthew Cook <cookm353@gmail.com>
2023-04-04 15:12:35 -0400

    Added CSS, implemented remove button

--- ToDos/src/ToDoList.js
+++ ToDos/src/ToDoList.js
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 import NewToDoForm from "./NewToDoForm"
 import ToDo from "./ToDo"
 import { v4 as uuidv4 } from "uuid"
+import "./ToDoList.css"
 
 const ToDoList = () => {
     const initialState = [
@@ -10,27 +11,28 @@ const ToDoList = () => {
         "Take shower",
         "Walk dog"
     ]
-
+    
     const [tasks, setTasks] = useState(initialState)
     const addTask = newTask => {
         setTasks(tasks => [...tasks, newTask])
     }
 
     const handleRemove = (e) => {
-        const button = e.target
-        
+        const bttn = e.target
+        const task = bttn.parentElement.parentElement
+        task.remove()
     }
     
     return (
-        <>
-            <NewToDoForm addTask={addTask} />
-            <div>
-                {tasks.map(task => {
-                    <ToDo task={task} id={uuidv4()} />
-                })}
-                <button onClick={handleRemove}>X</button>
-            </div>
-        </>
+        <div className="toDos">
+            {/* <NewToDoForm addTask={addTask} /> */}
+                {tasks.map(task => 
+                    <div className="task" key={uuidv4()}>
+                        <ToDo task={task} />
+                        <span><button onClick={handleRemove}>X</button></span>
+                    </div>
+                )}
+        </div>
     )
 }
 
